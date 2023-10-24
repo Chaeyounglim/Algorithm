a2b3614f5ca123316b57a5f2b65f721d34f2e178
Chaeyounglim <55676554+Chaeyounglim@users.noreply.github.com>
2023-10-24 15:48:26 +0900

    [level unrated] Title: 분수의 덧셈, Time: 0.09 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,27 +1,40 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
-        int[] answer = {0,0};
-        int denom, numer = 0;
+        int denom = denom1 * denom2;
+        int numer = (numer1*denom2) + (numer2*denom1);
         
-        if(denom1==denom2){
-            denom = denom1;
-            numer = numer1 + numer2;
-        }else {
-            denom = (denom1*denom2);
-            numer = (denom1*numer2) + (denom2*numer1);
-        }
-        
-        int denom_result = denom;
-        int numer_result = numer;
-        
-        for(int i =1; i<=denom; i++){
+        for(int i = denom ; i>=1; i--){
             if(denom%i==0 && numer%i==0){
-                denom_result = denom /i;
-                numer_result = numer /i;
+                denom /= i;
+                numer /= i;
+                break;
             }
         }
-        answer[0] = numer_result;
-        answer[1] = denom_result;
+        
+        
+        int[] answer = {numer,denom};
         return answer;
+        
+        // 방법1
+//         if(denom1==denom2){
+//             denom = denom1;
+//             numer = numer1 + numer2;
+//         }else {
+//             denom = (denom1*denom2);
+//             numer = (denom1*numer2) + (denom2*numer1);
+//         }
+        
+//         int denom_result = denom;
+//         int numer_result = numer;
+        
+//         for(int i =1; i<=denom; i++){
+//             if(denom%i==0 && numer%i==0){
+//                 denom_result = denom /i;
+//                 numer_result = numer /i;
+//             }
+//         }
+        
+//         int[] answer = {numer_result,denom_result};
+//         return answer;
     }
 }
