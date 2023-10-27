5898bcc73c1c0d127c367588562a89f829049f49
Chaeyounglim <55676554+Chaeyounglim@users.noreply.github.com>
2023-10-27 14:34:57 +0900

    [level unrated] Title: 세로 읽기, Time: 10.89 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
@@ -1,14 +1,11 @@
 class Solution {
     public String solution(String my_string, int m, int c) {
-        int length = my_string.length()%m==0 ? my_string.length()/m :my_string.length()/m+1;
         String answer = "";
-        String[] list = new String[length];
         
-        for(int i=0; i<length; i++){
-            answer += my_string.substring(0,m).charAt(c-1);
-            my_string = my_string.substring(m);
+        for(int i=c-1; i<my_string.length(); i+=m){
+            answer += my_string.charAt(i);
         }
-        
+
         return answer;
     }
 }
