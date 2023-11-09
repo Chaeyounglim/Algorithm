89b674c48338645b07b3f50e5950e73ef93a22d9
Chaeyounglim <55676554+Chaeyounglim@users.noreply.github.com>
2023-11-09 13:31:03 +0900

    [level unrated] Title: 9로 나눈 나머지, Time: 1.28 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -1,13 +1,5 @@
 class Solution {
     public int solution(String number) {
-        return number.chars().map(c->(c-'0')%9).sum()%9;
-            
-//         int eachSum = 0;
-        
-//         String[] list = number.split("");
-//         for(String str : list)
-//             eachSum += Integer.parseInt(str);
-        
-//         return eachSum%9;
+        return number.chars().map(c->(c-'0')).sum()%9;
     }
 }
