7582538ca133b83df5ea77c6be59e0b0d4a0b64f
Chaeyounglim <55676554+Chaeyounglim@users.noreply.github.com>
2023-11-09 14:49:19 +0900

    [level unrated] Title: 문자열 여러 번 뒤집기, Time: 3.89 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -1,7 +1,7 @@
 class Solution {
     public String solution(String my_string, int[][] queries) {
         StringBuffer sb = new StringBuffer(my_string);
-        
+
         for(int[] query : queries){
             StringBuffer temp = new StringBuffer(sb.substring(query[0],query[1]+1));
             temp.reverse();
