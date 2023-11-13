45303ea3eab7cc57f72c4ad5a7b22aa00fe0cdda
Chaeyounglim <55676554+Chaeyounglim@users.noreply.github.com>
2023-11-13 15:54:33 +0900

    [level 1] Title: 폰켓몬, Time: 3.05 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -6,14 +6,6 @@ class Solution {
         for(int i : nums){
             if(!list.contains(i)) list.add(i);
         }
-            System.out.println(list.toString());
-        
-        
-        // if((nums.length/2+1) == list.size()){
-        //     return list.size();
-        // }else {
-        //     list.size()+1;
-        // }
         return (nums.length/2 < list.size()) ? nums.length/2 : list.size() ;
     }
 }
