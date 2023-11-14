6ff78dadd4a341b34425b6338e3a6da22c1be80d
Chaeyounglim <55676554+Chaeyounglim@users.noreply.github.com>
2023-11-14 14:01:54 +0900

    [level unrated] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/unrated/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,17 +1,11 @@
 class Solution {
     public int solution(int chicken) {
-        // 서비스 치킨의 수
-        // 1마리당 쿠폰 1개
-        // 쿠폰 10개 치킨 한마리(쿠폰발급)
+        int answer = chicken/9;
         
-        int answer = 0;
-        
-        while(chicken>=10){
-            int newChick = chicken/10;
-            int restChick = chicken%10;
-            chicken = newChick + restChick;
-            answer+=newChick;
+        if(chicken>1&&chicken%9==0){
+            answer--;
         }
+
         return answer;
     }
 }
