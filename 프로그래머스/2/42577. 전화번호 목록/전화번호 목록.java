efeec94ef1f3f38836b1a00637ffdd5289bf8f2b
Chaeyounglim <55676554+Chaeyounglim@users.noreply.github.com>
2023-11-14 15:12:16 +0900

    [level 2] Title: 전화번호 목록, Time: 363.05 ms, Memory: 188 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,17 +1,16 @@
 import java.util.*;
 
 class Solution {
-    public boolean solution(String[] phone_book) {
+    public boolean solution(String[] phoneBook) {
         Set<String> setList = new HashSet<>();
         
-        for(String book : phone_book){
+        for(String book : phoneBook){
             setList.add(book);
         }
         
-        for(String book : phone_book){
-            for(int i=1; i<book.length(); i++){
+        for(String book : setList){
+            for(int i=1; i<book.length(); i++)
                 if(setList.contains(book.substring(0,i))) return false;
-            }
         }
         return true;
     }
